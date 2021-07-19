0dd1cfb6707beaf024e82012bac3dcf56096851e
김가영 Kim gayoung <rkdud91455@gmail.com>
2021-07-19 16:57:24 +0900

    docs: fix code
    fix code

--- 과제1/js/common.js
+++ 과제1/js/common.js
@@ -1,20 +1,31 @@
 $(function () {
   //dropdown navigation bar
-  // $("#nav .nav li").hover(
-  //   function () {
-  //     $(".nav_wrap").addClass("on");
-  //     $(".sub_gnb").slideDown();
-  //     $(".sub_gnb").hover(
-  //       function () {},
-  //       function () {
-  //         $(".sub_gnb").slideUp();
-  //       }
-  //     );
-  //   },
-  //   function () {
-  //     $(".nav_wrap").removeClass("on");
-  //   }
-  // );
+  $(".nav > li").hover(
+    function () {
+      $(this).find("a").addClass("on");
+      $(this).find(".sub_gnb").addClass("on");
+      $(".header_bg").addClass("on");
+      // $(".header_bg").hover(
+      //   function () {},
+      //   function () {
+      //     $(".header_bg").removeClass("on");
+      //   }
+      // );
+      // $(this)
+      //   .find(".sub_gnb")
+      //   .hover(
+      //     function () {},
+      //     function () {
+      //       $(this).find(".sub_gnb").removeClass("on");
+      //     }
+      //   );
+    },
+    function () {
+      $(this).find("a").removeClass("on");
+      $(this).find(".sub_gnb").removeClass("on");
+      $(".header_bg").removeClass("on");
+    }
+  );
 
   //unactive a tag
   $('a[href="#"]').click(function (e) {
@@ -22,10 +33,21 @@ $(function () {
   });
 
   //back to top
-  $(".back_to_top a").click(function () {
+  $(".back_to_top").click(function () {
     let thisElem = $(this.hash);
     let offsetElem = thisElem.offset();
     $("html,body").stop();
-    $("html,body").animate({ scrollTop: offsetElem.top }, 1000);
+    $("html,body").animate({ scrollTop: offsetElem.top }, 700);
+  });
+
+  //footer icon
+  $(".cooperation").click(function () {
+    if ($(".cooperation_list").height() == 0) {
+      $(".cooperation_list").addClass("on");
+      $(this).addClass("on");
+    } else {
+      $(".cooperation_list").removeClass("on");
+      $(this).removeClass("on");
+    }
   });
 });
